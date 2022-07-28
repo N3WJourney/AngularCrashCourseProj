5206a4287fb14292d8bb93c80bcdcedd319c72e2
N3WJourney <gary.alan.smith1@gmail.com>
2022-07-27 20:17:27 -0400

    Complete course application - Good practice

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,14 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
   title: string = 'Task Tracker';
-  constructor() { }
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription;
+
+  constructor(private uiService:UiService, private route:Router) {
+    this.subscription = this.uiService.onToggleForm().subscribe((val) => {
+      this.showAddTask = val;
+    });
+   }
 
   ngOnInit(): void {
   }
 
   ToggleAddForm() {
-    console.log('Toggled form');
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string)  {
+    return this.route.url === route;
   }
 
 }
